f2ad8df8287f5eddb1602dfefa76efca8f5f9b5a
sefzaa <2011521016_sefza@student.unand.ac.id>
2026-07-04 10:51:39 +0700

    feat: update Swagger host to production URL

--- docs/docs.go
+++ docs/docs.go
@@ -1433,7 +1433,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "pamagi.mydm.cloud", //pamagi.mydm.cloud localhost:8080
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Pamagi API",
